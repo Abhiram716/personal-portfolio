6365aa2d4e6a6c4c1f86d15edc463310dbf99276
Abhiram Kantipudi <a4abhiram125@gmail.com>
2023-07-11 18:53:01 +0530

    updated container style

--- src/theme/overrides/Container.js
+++ src/theme/overrides/Container.js
@@ -3,6 +3,9 @@ const ContainerStyles = {
     root: {
       background:
         "linear-gradient(180deg, #010208 16.39%, #010206 20.51%, #040008 24.79%, #010101 30.46%, #010101 34.11%, #010101 36.21%, #010101 100%)",
+      paddingTop: "316px",
+      paddingRight: "112px",
+      paddingLeft:"112px"
     },
   },
 };
