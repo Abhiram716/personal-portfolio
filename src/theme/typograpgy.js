b95651d923ec06eedf283bc71087fdb1180bd6f6
Abhiram Kantipudi <a4abhiram125@gmail.com>
2023-07-17 09:39:49 +0530

    feat: created homepage component

--- src/theme/typograpgy.js
+++ src/theme/typograpgy.js
@@ -7,8 +7,21 @@ const Typography = {
     "Arial",
     "sans-serif",
   ].join(","),
+  h2: {
+    fontWeight: "900",
+    fontWeight: "900",
+    fontSize: "4rem",
+    "@media (max-width:800px)": {
+      fontSize: "3rem",
+    },
+  },
   h4: {
     fontWeight: "900",
+  },
+  h5: {
+    "@media (max-width:600px)": {
+      fontSize: "1.2rem",
+    },
   },
   h6: {
     fontWeight: "900",
