1dd285dce5e60223fef05fd296aa54089f30f9ec
Abhiram Kantipudi <78171758+Abhiram716@users.noreply.github.com>
2023-07-29 22:04:26 +0530

    Revert "Enhancing Home Page" (#9)

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,31 +6,5 @@ module.exports = {
     title: `Personal portfolio`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    {
-      resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
-      options: {
-        devMode: true,
-      },
-    },
-    {
-      resolve: "gatsby-omni-font-loader",
-      options: {
-        mode: "async",
-        enableListener: true,
-        preconnect: ["https://fonts.gstatic.com"],
-        web: [
-          {
-            name: "Poppins",
-            file: "https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap",
-          },
-          {
-            name: "Kurale",
-            file: "https://fonts.googleapis.com/css2?family=Kurale&display=swap",
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-optimize-svgs`,
-  ],
-};
+  plugins: [],
+}
