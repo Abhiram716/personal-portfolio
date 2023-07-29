c6c320c79a8e6752f44f08a5f35b5fc78268de1c
Abhiram Kantipudi <78171758+Abhiram716@users.noreply.github.com>
2023-07-30 00:31:58 +0530

    Revert "Revert "Enhancing Home Page" (#9)" (#11)
    This reverts commit 1dd285dce5e60223fef05fd296aa54089f30f9ec.

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,6 +1,4 @@
 import { createTheme } from "@mui/material/styles";
-import "@fontsource/poppins/300.css";
-import "@fontsource/kurale";
 
 import Components from "./overrides";
 import Typography from "./typograpgy";
