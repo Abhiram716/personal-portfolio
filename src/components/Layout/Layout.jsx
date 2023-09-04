9aa0270102b58ad3fb0c497e0623d4e00fdb672f
Abhiram Kantipudi <a4abhiram125@gmail.com>
2023-09-04 08:39:39 +0530

    feat:added scroll feature

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
+import "../../styles/global.css";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <>{children}</>
+      {children}
       <Footer />
     </>
   );
